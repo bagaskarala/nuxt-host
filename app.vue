2565cf511e1eb60d2e76a13a281ad893649aa6a1
Bagaskara LA <bagaskaraluthfi@gmail.com>
2024-09-25 15:34:39 +0700

    add embedded widget

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
     Host app
+    <NuxtPage />
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    Inject embedded widget below
+    <div id="script-nuxt-embed"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+onMounted(() => {
+  window.embedEvfScript = function () {
+    !(function (w, d, e, v, id, t, s, x, y) {
+      x = d.createElement('div');
+      x.id = id;
+      x.innerHTML = 'Loading Widget...';
+      y = d.getElementById('script-nuxt-embed');
+      y.parentNode.insertBefore(x, y);
+
+      t = d.createElement(e);
+      t.async = !0;
+      t.src = v;
+      s = d.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t, s);
+    })(
+      window,
+      document,
+      'script',
+      'http://localhost:3000/bundle.js',  // bundle generated form Nuxt 3 SSG -> npm run generate
+      'nuxt-embed-app'
+    );
+  };
+
+  window.embedEvfScript();
+});
+</script>
